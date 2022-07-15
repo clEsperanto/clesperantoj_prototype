c3ea258a56afd2398be6c75ce1a20b7a6424b12a
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-07-15 21:41:09 +0200

    enable pushing 3D Img

---
+++ src/main/java/net/clesperanto/converters/implementations/RandomAccessibleIntervalToObjectJConverter.java
@@ -0,0 +1,97 @@
+package net.clesperanto.converters.implementations;
+
+import net.clesperanto.clicwrapper.ConvertersUtility;
+import net.clesperanto.clicwrapper.clesperantojWrapper;
+import net.clesperanto.converters.AbstractConverter;
+import net.clesperanto.converters.ConverterPlugin;
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.converters.AbstractCLIJConverter;
+import net.haesleinhuepf.clij.converters.ConverterUtilities;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.type.numeric.integer.ShortType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.Views;
+import org.bytedeco.javacpp.FloatPointer;
+import org.scijava.plugin.Plugin;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import static net.clesperanto.pyclesperanto.cle;
+
+@Plugin(type = ConverterPlugin.class)
+public class RandomAccessibleIntervalToObjectJConverter extends AbstractConverter<RandomAccessibleInterval, clesperantojWrapper.ObjectJ> {
+
+    @Override
+    public clesperantojWrapper.ObjectJ convert(RandomAccessibleInterval source) {
+        return convert_int(source);
+    }
+
+    private <T extends RealType<T>> clesperantojWrapper.ObjectJ convert_int(RandomAccessibleInterval source) {
+        long[] dimensions = new long[source.numDimensions()];
+        source.dimensions(dimensions);
+
+        long[] dimensions3d = {1,1,1};
+        long numberOfPixels = 1;
+        for (int i = 0; i < dimensions.length; i++) {
+            numberOfPixels *= dimensions[i];
+            dimensions3d[i] = dimensions[i];
+        }
+        RealType pixel = (RealType) (Views.iterable(source).firstElement());
+
+        int count = 0;
+        Cursor<T> cursor = Views.iterable(source).cursor();
+
+        if (pixel instanceof UnsignedByteType ||
+            pixel instanceof ByteType) {
+
+            byte[] inputArray = new byte[(int) numberOfPixels];
+            while (cursor.hasNext()) {
+                inputArray[count] = (byte) cursor.next().getRealFloat();
+                count++;
+            }
+            ByteBuffer byteBuffer = ByteBuffer.wrap(inputArray);
+            return cle._native.CharPush(byteBuffer, (int)dimensions3d[0], (int)dimensions3d[1], (int)dimensions3d[2]);
+            //target.readFrom(byteBuffer, true);
+        } else if (pixel instanceof ShortType ||
+                   pixel instanceof UnsignedShortType) {
+
+            short[] inputArray = new short[(int) numberOfPixels];
+            while (cursor.hasNext()) {
+                inputArray[count] = (short) cursor.next().getRealFloat();
+                count++;
+            }
+            ShortBuffer shortBuffer = ShortBuffer.wrap(inputArray);
+            return cle._native.ShortPush(shortBuffer, (int)dimensions3d[0], (int)dimensions3d[1], (int)dimensions3d[2]);
+            //target.readFrom(shortBuffer, true);
+        } else if (pixel instanceof FloatType) {
+            float[] inputArray = new float[(int) numberOfPixels];
+            while (cursor.hasNext()) {
+                inputArray[count] = cursor.next().getRealFloat();
+                count++;
+            }
+            FloatBuffer floatBuffer = FloatBuffer.wrap(inputArray);
+            return cle._native.FloatPush(floatBuffer, (int)dimensions3d[0], (int)dimensions3d[1], (int)dimensions3d[2]);
+            //target.readFrom(floatBuffer, true);
+        } else {
+            throw new IllegalArgumentException("Cannot copy content of buffer because of unknown type.");
+        }
+    }
+
+    @Override
+    public Class<RandomAccessibleInterval> getSourceType() {
+        return RandomAccessibleInterval.class;
+    }
+
+    @Override
+    public Class<clesperantojWrapper.ObjectJ> getTargetType() {
+        return clesperantojWrapper.ObjectJ.class;
+    }
+}

e9183f4b0f5dc16d0d4ad4e68613f5dfd0d4e6df
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-07-15 22:02:04 +0200

    added Img ImagePlus converters for completeness

---
+++ src/main/java/net/clesperanto/converters/implementations/RandomAccessibleIntervalToImagePlusConverter.java
@@ -0,0 +1,29 @@
+package net.clesperanto.converters.implementations;
+
+import ij.ImagePlus;
+import net.clesperanto.converters.AbstractConverter;
+import net.clesperanto.converters.ConverterPlugin;
+import net.haesleinhuepf.clij.converters.AbstractCLIJConverter;
+import net.haesleinhuepf.clij.converters.CLIJConverterPlugin;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = ConverterPlugin.class)
+public class RandomAccessibleIntervalToImagePlusConverter extends AbstractConverter<RandomAccessibleInterval, ImagePlus> {
+
+    @Override
+    public ImagePlus convert(RandomAccessibleInterval source) {
+        return ImageJFunctions.wrap(source, "" + source);
+    }
+
+    @Override
+    public Class<RandomAccessibleInterval> getSourceType() {
+        return RandomAccessibleInterval.class;
+    }
+
+    @Override
+    public Class<ImagePlus> getTargetType() {
+        return ImagePlus.class;
+    }
+}

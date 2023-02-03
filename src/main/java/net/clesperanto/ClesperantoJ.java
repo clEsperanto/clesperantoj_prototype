1087c0c2b054bfd2f759cf1e23fd29e7fce777d5
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-02-03 18:21:05 +0100

    build pass but cannot run

--- src/main/java/net/clesperanto/ClesperantoJ.java
+++ src/main/java/net/clesperanto/ClesperantoJ.java
@@ -1,104 +1,13 @@
 package net.clesperanto;
 
-import ij.ImagePlus;
-import net.clesperanto.clicwrapper.clesperantojWrapper;
-import net.clesperanto.converters.ConverterPlugin;
-import net.clesperanto.converters.ConverterService;
-import net.imglib2.RandomAccessibleInterval;
-import org.scijava.Context;
+import net.clesperanto.clicwrapper.clesperantojWrapper.ClesperantoJInternal;
 
 public class ClesperantoJ {
 
-    @Deprecated
-    public ClesperantoJ() {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Native Java");
 
-    }
-
-    private static ClesperantoJ instance = null;
-    public static ClesperantoJ getInstance() {
-        if (instance == null) {
-            instance = new ClesperantoJ();
-        }
-        return instance;
-    }
-
-    public clesperantojWrapper.ClesperantoJInternal _native = new clesperantojWrapper.ClesperantoJInternal();
-
-    public void say_hello() {
+        ClesperantoJInternal _native = new ClesperantoJInternal();
         _native.sayHello();
-    }
-
-    public clesperantojWrapper.ObjectJ push(Object image) {
-        return convert(image, clesperantojWrapper.ObjectJ.class);
-    }
-
-    public ImagePlus pull(Object image) {
-        return convert(image, ImagePlus.class);
-    }
-
-    public RandomAccessibleInterval pullRAI(Object image) {
-        return convert(image, RandomAccessibleInterval.class);
-    }
-
-
-    private ConverterService converterService = null;
-
-    public <S, T> T convert(S source, Class<T> targetClass) {
-        if (source == null) {
-            return null;
-        }
-        if (targetClass.isAssignableFrom(source.getClass())) {
-            return (T) source;
-        }
-        synchronized (this) {
-            //try {
-                if (converterService == null) {
-                    converterService = new Context(ConverterService.class).service(ConverterService.class);
-                }
-            //} catch (RuntimeException e) {
-            //    converterService = FallBackCLIJConverterService.getInstance();
-            //}
-            converterService.setCLE(this);
-            ConverterPlugin<S, T> converter = (ConverterPlugin<S, T>) converterService.getConverter(source.getClass(), targetClass);
-            converter.setCLE(this);
-            T result = converter.convert(source);
-
-            return  result;
-        }
-    }
-
-    public void imshow(Object gpu_image) {
-        ImagePlus image = pull(gpu_image);
-        image.show();
-    }
-
-    public clesperantojWrapper.ObjectJ create_like(clesperantojWrapper.ObjectJ source) {
-        return _native.FloatCreate(source.getWidth(), source.getHeight(), source.getDepth());
-    }
-
-    private clesperantojWrapper.ObjectJ create_like_if_none(Object source, Object target) {
-        clesperantojWrapper.ObjectJ sourceJ = push(source); // that might be not necessary and slow
-        clesperantojWrapper.ObjectJ targetJ = push(target); // that might be not necessary and slow
-        if (targetJ != null) {
-            return targetJ;
-        }
-        return create_like(sourceJ);
-    }
-
-    public clesperantojWrapper.ObjectJ gaussian_blur(Object source, Object target, float sigma_x, float sigma_y, float sigma_z) {
-        clesperantojWrapper.ObjectJ sourceJ = push(source);
-        clesperantojWrapper.ObjectJ targetJ = create_like_if_none(sourceJ, target);
-        return _native.gaussian_blur(sourceJ, targetJ, sigma_x, sigma_y, sigma_z);
-    }
-    public clesperantojWrapper.ObjectJ thresold_otsu(Object source, Object target) {
-        clesperantojWrapper.ObjectJ sourceJ = push(source);
-        clesperantojWrapper.ObjectJ targetJ = create_like_if_none(sourceJ, target);
-        return _native.threshold_otsu(sourceJ, targetJ);
-    }
-
-    public clesperantojWrapper.ObjectJ connected_component_labeling_box(Object source, Object target) {
-        clesperantojWrapper.ObjectJ sourceJ = push(source);
-        clesperantojWrapper.ObjectJ targetJ = create_like_if_none(sourceJ, target);
-        return _native.connected_component_labeling_box(sourceJ, targetJ);
     }
 }
--- src/main/java/net/clesperanto/clicwrapper/clesperantoj.java
+++ src/main/java/net/clesperanto/clicwrapper/clesperantoj.java
@@ -7,34 +7,38 @@ import org.bytedeco.javacpp.tools.*;
 // java cpp configuration class that will enable the parser to create a wrapper class in src/gen/java
 // see https://github.com/bytedeco/javacpp/wiki/Mapping-Recipes
 @Properties(value = { @Platform(
-	// note: comment this in to debug
-	compiler = {"fastfpu","debug"},
-	// header file for c++ class we want to wrap
-	include = "clesperantoj.h",
-	// libraries we need to link to
-	link = {"clesperantoj", "CLIc_d", "OpenCL" }),
-	// os specific properties (note relative include and library paths are specified in the pom)	
-	@Platform(value = "windows-x86_64",
-		includepath = {
-			"C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/include/"},	 
-		linkpath = {
-			"C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/lib/x64/"
-		}),
-//	@Platform(value = "linux-x86_64",
-//			includepath = {"/usr/local/cuda-10.0/include/"},
-//			linkpath = {
-//			"/usr/local/cuda-10.0/lib64/" },
-//			preload = { "clFFT" }) 
+		// note: comment this in to debug
+		compiler = { "fastfpu", "release" },
+		// header file for c++ class we want to wrap
+		include = "clesperantoj.hpp",
+		// libraries we need to link to
+		link = { "clesperantoj", "CLIc", "OpenCL" }),
+		// os specific properties (note relative include and library paths are specified
+		// in the pom)
+		// @Platform(value = "windows-x86_64", includepath = {
+		// "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/include/" },
+		// linkpath = {
+		// "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/lib/x64/"
+		// }),
+		@Platform(value = "linux-x86_64",
+				// includepath = {"/usr/local/cuda-10.0/include/"},
+				linkpath = {
+						"/usr/lib/x86_64-linux-gnu/" }
+		// preload = { "clFFT" }
+		)
 },
-// name of java wrapper class that will be generated (look in src/gen/java to find it) 
-target = "net.clesperanto.clicwrapper.clesperantojWrapper" 
-)
+		// name of java wrapper class that will be generated (look in src/gen/java to
+		// find it)
+		target = "net.clesperanto.clicwrapper.clesperantojWrapper")
 public class clesperantoj implements InfoMapper {
 	public void map(InfoMap infoMap) {
-		
-		// the parser will try to automatically make wrappers for all functions in cleperantoj.h
-		// however templates need to be handled specially, and we need to set properties in the info map
-		// that specify the name of the wrapper to each template type we want to generate
+
+		// the parser will try to automatically make wrappers for all functions in
+		// cleperantoj.h
+		// however templates need to be handled specially, and we need to set properties
+		// in the info map
+		// that specify the name of the wrapper to each template type we want to
+		// generate
 		infoMap.put(new Info("ClesperantoJInternal::guassianBlur2dT<float>").javaNames("FloatGaussianBlur2dT"));
 		infoMap.put(new Info("ClesperantoJInternal::guassianBlur2dT<short>").javaNames("ShortGaussianBlur2dT"));
 		infoMap.put(new Info("ClesperantoJInternal::create<char>").javaNames("CharCreate"));

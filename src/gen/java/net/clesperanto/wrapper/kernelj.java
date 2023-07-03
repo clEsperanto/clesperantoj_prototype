ff244a6b03e08730cc71bb8154758c654d4c8ed2
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-03 15:15:41 +0200

    auto gen files ...

--- src/gen/java/net/clesperanto/wrapper/kernelj.java
+++ src/gen/java/net/clesperanto/wrapper/kernelj.java
@@ -36,7 +36,8 @@ public static class Tier1 extends Pointer {
         return new Tier1((Pointer)this).offsetAddress(i);
     }
 
-    public static native void addImageAndScalar(@Const @ByRef ProcessorJ proc, @Const @ByRef BufferJ src, @Const @ByRef BufferJ dst, float scalar);
+    public static native void absolute(@Const @ByRef DeviceJ dev, @Const @ByRef ArrayJ src, @Const @ByRef ArrayJ dst);
+    public static native void gaussianBlur(@Const @ByRef DeviceJ dev, @Const @ByRef ArrayJ src, @Const @ByRef ArrayJ dst, float sigmaX, float sigmaY, float sigmaZ);
 }
 
 // #endif // __INCLUDE_KERNELJ_HPP

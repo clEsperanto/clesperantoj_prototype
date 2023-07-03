026b474632bed5686f35190c4e39d7271a4924d8
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-03 15:15:33 +0200

    test new code

--- src/main/java/net/clesperanto/ClesperantoJ.java
+++ src/main/java/net/clesperanto/ClesperantoJ.java
@@ -4,85 +4,59 @@ import net.clesperanto.wrapper.clesperantoj.StringVector;
 
 import java.util.Arrays;
 
+import org.bytedeco.javacpp.FloatPointer;
 import org.bytedeco.javacpp.LongPointer;
+import org.bytedeco.javacpp.Pointer;
 
-import net.clesperanto.wrapper.clesperantoj.ProcessorJ;
-import net.clesperanto.wrapper.kernelj.Tier1;
-import net.clesperanto.wrapper.clesperantoj.BufferJ;
+import net.clesperanto.wrapper.clesperantoj.DeviceJ;
 import net.clesperanto.wrapper.clesperantoj.MemoryJ;
+import net.clesperanto.wrapper.clesperantoj.BackendJ;
+import net.clesperanto.wrapper.clesperantoj.ArrayJ;
+
+import net.clesperanto.wrapper.kernelj.Tier1;
 
 public class ClesperantoJ {
 
     public static void main(String[] args) {
         System.out.println("Hello World! Native Java");
 
-        try (ProcessorJ proc = new ProcessorJ()) {
-            StringVector deviceList = proc.getAvailableDevices();
-            for (int i = 0; i < deviceList.size(); i++) {
-                System.out.println(deviceList.get(i) + " is available");
-            }
-            System.out.println("device currently used is : " + proc.getDevice());
+        BackendJ.setBackend("opencl");
+        BackendJ.setBackend("cuda");
 
-            proc.setDevice("Intel");
-            System.out.println("device currently used is : " + proc.getDevice());
+        StringVector deviceList = DeviceJ.getAvailableDevices();
+        for (int i = 0; i < deviceList.size(); i++) {
+            System.out.println(deviceList.get(i) + " is available");
         }
 
-        try (BufferJ buffer = new BufferJ()) {
-            System.out.println("Buffer created");
+        try (DeviceJ currentDevice = new DeviceJ()) {
+            System.out.println("Device created");
+            currentDevice.setDevice("TX", "all");
+            System.out.println("device currently used is : " + currentDevice.getName());
+            System.out.println(currentDevice.getInfo());
         }
 
-        ProcessorJ proc = new ProcessorJ("TX");
+        DeviceJ currentDevice = new DeviceJ();
+        currentDevice.setDevice("TX", "all");
 
-        try (BufferJ buffer = MemoryJ.makeFloatBuffer(proc, 3, 3, 2, "buffer")) {
-
-            System.out.println("Buffer created");
-            System.out.println("Buffer shape is (" + buffer.getWidth() + ", " + buffer.getHeight() + ", "
-                    + buffer.getDepth() + ")");
-            System.out.println("Buffer data type is " + buffer.getDataType());
-            System.out.println("Buffer mem type is " + buffer.getMemoryType());
-            System.out.println("Buffer device is " + buffer.getDevice());
-
-            float data[] = new float[3 * 3 * 2];
-            float out[] = new float[3 * 3 * 2];
-            Arrays.fill(data, 5);
-            Arrays.fill(out, 1);
-
-            for (int i = 0; i < out.length; i++) {
-                System.out.println("before out[" + i + "] = " + out[i]);
-            }
-
-            MemoryJ.writeFloatBuffer(buffer, data, (long) data.length);
-            MemoryJ.readFloatBuffer(buffer, out, (long) out.length);
-
-            for (int i = 0; i < out.length; i++) {
-                System.out.println("after out[" + i + "] = " + out[i]);
-            }
-
-            buffer.fillMemory(10.0f);
-            MemoryJ.readFloatBuffer(buffer, out, (long) out.length);
-            for (int i = 0; i < out.length; i++) {
-                System.out.println("out[" + i + "] = " + out[i]);
-            }
-        }
-
-        BufferJ input = MemoryJ.makeFloatBuffer(proc, 3, 3, 2, "buffer");
-        BufferJ output = MemoryJ.makeFloatBuffer(proc, 3, 3, 2, "buffer");
+        ArrayJ input = MemoryJ.makeFloatBuffer(currentDevice, 3, 3, 2, "buffer");
+        ArrayJ output = MemoryJ.makeFloatBuffer(currentDevice, 3, 3, 2, "buffer");
 
         float data[] = new float[3 * 3 * 2];
         float out[] = new float[3 * 3 * 2];
-        Arrays.fill(data, 5);
-        Arrays.fill(out, 1);
+        Arrays.fill(data, -5);
+        Arrays.fill(out, -1);
 
         MemoryJ.writeFloatBuffer(input, data, (long) data.length);
         MemoryJ.writeFloatBuffer(output, out, (long) out.length);
 
-        Tier1.addImageAndScalar(proc, input, output, 10.0f);
+        Tier1.absolute(currentDevice, input, output);
 
         MemoryJ.readFloatBuffer(output, out, (long) out.length);
 
         for (int i = 0; i < out.length; i++) {
             System.out.println("out[" + i + "] = " + out[i]);
         }
+
     }
 
 }

12464b4e7c1dfa25738a2538785d6742ea0c4f07
carlosuc3m <100329787@alumnos.uc3m.es>
2024-06-20 18:47:35 +0200

    fill arrayJ class of core

--- src/main/java/net/clesperanto/core/ArrayJ.java
+++ src/main/java/net/clesperanto/core/ArrayJ.java
@@ -1,46 +1,104 @@
 package net.clesperanto.core;
 
+/**
+ * Class to interact with the arrays allocated on the GPU by ClesperantoJ
+ */
+/**
+ * TODO think about possible new methods
+ * 
+ * - method to modify the data inside the array from this class
+ * - Do we check here if the size of the destination array is compatible for the method {@link #copyDataTo(ArrayJ)}
+ * 
+ */
 public class ArrayJ {
 	
-	private net.clesperanto.wrapper.clesperantoj.ArrayJ arrayj;
+	protected net.clesperanto.wrapper.clesperantoj.ArrayJ arrayj;
 
     protected ArrayJ(net.clesperanto.wrapper.clesperantoj.ArrayJ arrayj) {
     	this.arrayj = arrayj;
     }
     
+    /**
+     * 
+     * @return the width of the array
+     */
     public long getWidth() {
     	return arrayj.getWidth();
     }
     
+    /**
+     * 
+     * @return the height of the array
+     */
     public long getHeight() {
     	return arrayj.getHeight();
     }
     
+    /**
+     * 
+     * @return the dimensions of the array. The order of the dimensions is [width, height, depth].
+     */
+    public long[] getDimensions() {
+    	return new long[] {this.getWidth(), this.getHeight(), this.getDepth()};
+    }
+    
+    /**
+     * 
+     * @return the depth of the array
+     */
     public long getDepth() {
     	return arrayj.getDepth();
     }
     
+    /**
+     * 
+     * @return the number of dimensions of the array
+     */
     public int getNDimensions() {
     	return arrayj.getDimension();
     }
     
+    /**
+     * 
+     * @return the data type of the array
+     */
     public String getDataType() {
     	return arrayj.getDataType();
     }
     
+    /**
+     * 
+     * @return the type of object that the array represents, either an 'image' or a 'buffer'
+     */
     public String getMemoryType() {
     	return arrayj.getMemoryType();
     }
     
+    /**
+     * 
+     * @return the name of the device where the array is allocated
+     */
     public String getDevice() {
     	return arrayj.getDevice();
     }
     
+    /**
+     * Fill the array in the GPU with the wanted value.
+     * All the positions in the array will be set to the same value
+     * @param value
+     * 	the value that every position in the array will adopt
+     */
     public void fillMemory(float value) {
     	this.arrayj.fillMemory(value);
     }
     
-    public void fillMemory(ArrayJ dst) {
+    /*
+     * Copy the data from the current array to the destination array given as a parameter
+     * 
+     * @param dst
+     * 	the array into which the current array will be copied
+     */
+    public void copyDataTo(ArrayJ dst) {
     	this.arrayj.copyDataTo(dst.arrayj);
     }
 }

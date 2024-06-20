411a72b6c39a7f5498811ae537acc20cd6baeafd
carlosuc3m <100329787@alumnos.uc3m.es>
2024-06-20 16:19:20 +0200

    add methods for arrayj

--- src/main/java/net/clesperanto/core/ArrayJ.java
+++ src/main/java/net/clesperanto/core/ArrayJ.java
@@ -1,9 +1,46 @@
 package net.clesperanto.core;
 
-
 public class ArrayJ {
+	
+	private net.clesperanto.wrapper.clesperantoj.ArrayJ arrayj;
 
-    public static void main(String[] args) {
+    protected ArrayJ(net.clesperanto.wrapper.clesperantoj.ArrayJ arrayj) {
+    	this.arrayj = arrayj;
     }
-
+    
+    public long getWidth() {
+    	return arrayj.getWidth();
+    }
+    
+    public long getHeight() {
+    	return arrayj.getHeight();
+    }
+    
+    public long getDepth() {
+    	return arrayj.getDepth();
+    }
+    
+    public int getNDimensions() {
+    	return arrayj.getDimension();
+    }
+    
+    public String getDataType() {
+    	return arrayj.getDataType();
+    }
+    
+    public String getMemoryType() {
+    	return arrayj.getMemoryType();
+    }
+    
+    public String getDevice() {
+    	return arrayj.getDevice();
+    }
+    
+    public void fillMemory(float value) {
+    	this.arrayj.fillMemory(value);
+    }
+    
+    public void fillMemory(ArrayJ dst) {
+    	this.arrayj.copyDataTo(dst.arrayj);
+    }
 }

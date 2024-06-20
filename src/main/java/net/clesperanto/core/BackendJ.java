cbc7274a6acfdb7813a1aa69013f8de165774c7b
carlosuc3m <100329787@alumnos.uc3m.es>
2024-06-20 14:30:58 +0200

    fill the backendJ class with methods from JAvacpp

--- src/main/java/net/clesperanto/core/BackendJ.java
+++ src/main/java/net/clesperanto/core/BackendJ.java
@@ -1,9 +1,18 @@
 package net.clesperanto.core;
 
 
+/**
+ * Class that contains the methods to interact and change the type of backend used for Clesperanto.
+ */
 public class BackendJ {
 
-    public static void main(String[] args) {
+	/**
+	 * Set the type of backend that Clesperanto is going to use.
+	 * @param backend
+	 * 	the type of backend that wants to be used. It should be either "cuda" or "opencl",
+	 * 	if it is anything else it will be set to "opencl"
+	 */
+    public static void setBackend(String backend) {
+    	net.clesperanto.wrapper.clesperantoj.BackendJ.setBackend(backend);
     }
-
 }

915141e222b33c6a6be1fa3833c05f1bc9f75f77
carlosuc3m <100329787@alumnos.uc3m.es>
2024-06-21 11:39:20 +0200

    create the methods to convert from ArrayJ into ImgLib2 and viceversa

--- src/main/java/net/clesperanto/imglib2/Converters.java
+++ src/main/java/net/clesperanto/imglib2/Converters.java
@@ -1,9 +1,13 @@
 package net.clesperanto.imglib2;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 import net.clesperanto.core.ArrayJ;
+import net.clesperanto.core.DeviceJ;
 import net.clesperanto.core.MemoryJ;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.blocks.PrimitiveBlocks;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.basictypeaccess.nio.BufferAccess;
 import net.imglib2.img.basictypeaccess.nio.BufferDataAccessFactory;
@@ -15,12 +19,13 @@ import net.imglib2.util.Fraction;
 /**
  * TODO 
  * - define whether the buffers follow big or little endian and whether the copies are c- or fortran order
+ * - using array img and blocks copy limits to images whose array size is smaller than the integer max
  */
 public class Converters {
 	
 	private static String FLOAT32 = "float32";
 	
-	public static < T extends NativeType< T >, A extends BufferAccess< A > > ArrayImg< T, A > asImgLib2ArrayImg(
+	public static < T extends NativeType< T >, A extends BufferAccess< A > > ArrayImg< T, A > arrayJToImgLib2(
 			ArrayJ arrayj )
 	{
 		String dType = arrayj.getDataType();
@@ -37,6 +42,25 @@ public class Converters {
 		}
 	}
 	
+	public static < T extends NativeType< T > > ArrayJ imgLib2ToArrayJ(RandomAccessibleInterval<T> rai, DeviceJ device, String memoryType) {
+		checkSize(rai);
+		PrimitiveBlocks< T > blocks = PrimitiveBlocks.of( rai );
+		long totalSize = Arrays.stream(rai.dimensionsAsLongArray()).reduce(1L, (a, b) -> a * b);
+		if (totalSize > Integer.MAX_VALUE)
+			throw new IllegalArgumentException();
+		int[] integerDims = Arrays.stream(rai.dimensionsAsLongArray()).mapToInt(x -> (int) x).toArray();
+		
+		if (rai.getType() instanceof FloatType) {
+			float[] flatArr = new float[(int) totalSize];
+			blocks.copy( new int[rai.dimensionsAsLongArray().length], flatArr, integerDims );
+			ArrayJ arrayJ = MemoryJ.makeFloatBuffer(device, integerDims[0], integerDims[1], integerDims[2], totalSize, memoryType);
+			MemoryJ.writeFloatBuffer(arrayJ, flatArr, 0);
+			return arrayJ;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+	
 	private static < T extends NativeType< T >, A extends BufferAccess< A > > ArrayImg< T, A > 
 		fromBuffer(ByteBuffer byteBuffer, T type, long[] dimensions) {
 
@@ -48,6 +72,14 @@ public class Converters {
 		final ArrayImg< T, A > img = new ArrayImg<>( data, dimensions, entitiesPerPixel );
 		img.setLinkedType( typeFactory.createLinkedType( img ) );
 		return img;
+	}
+	
+	private static < T extends NativeType< T > > void checkSize(RandomAccessibleInterval<T> rai) {
+		long[] dims = rai.dimensionsAsLongArray();
+		for (long l : dims) {
+			if (l > Integer.MAX_VALUE)
+				throw new IllegalArgumentException();
+		}
 	}
 
 }

b0e3ca72819088ea6b287fe9ebb09949717365df
carlosuc3m <100329787@alumnos.uc3m.es>
2024-06-28 02:22:27 +0200

    design possible structures

---
+++ src/main/java/net/clesperanto/kernels/Tier1.java
@@ -0,0 +1,32 @@
+package net.clesperanto.kernels;
+
+import net.clesperanto.core.ArrayJ;
+
+public class Tier1 {
+	
+	
+	public static ArrayJ absolute(DeviceJ, ArrayJ input) {
+		
+	}
+	
+	
+	public static void absolute(DeviceJ, ArrayJ input, ArrayJ output) {
+		
+	}
+	
+	
+	public static Object absolute(DeviceJ, Object input) {
+		
+	}
+	
+	
+	public static ArrayJ absolute(DeviceJ, Object input) {
+		
+	}
+	
+	
+	public static Object absolute(DeviceJ, Object input, String returnType) {
+		
+	}
+
+}

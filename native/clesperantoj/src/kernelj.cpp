c8f57447d65e66c3308dfb1c5ae7c15d43d199d2
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-07-24 16:17:59 +0200

    update native code to ptr

--- native/clesperantoj/src/kernelj.cpp
+++ native/clesperantoj/src/kernelj.cpp
@@ -1,25 +1,18 @@
 #include "kernelj.hpp"
 #include "tier1.hpp"
 
-ArrayJ Tier1::absolute(const DeviceJ &dev, const ArrayJ &src, ArrayJ &dst)
+ArrayJ Tier1::absolute( DeviceJ* dev,  ArrayJ* src, ArrayJ* dst)
 {
-    auto out = cle::tier1::absolute_func(dev.get(), src.get(), dst.get());
-    dst = ArrayJ{out};
-    return dst;
+    return ArrayJ{cle::tier1::absolute_func(dev->get(), src->get(), dst ? dst->get() : nullptr)};
 }
 
-ArrayJ Tier1::absolute(const DeviceJ &dev, const ArrayJ &src)
+ArrayJ Tier1::gaussianBlur(DeviceJ* dev, ArrayJ* src, ArrayJ *dst, float sigmaX, float sigmaY, float sigmaZ)
 {
-    auto out = cle::tier1::absolute_func(dev.get(), src.get(), nullptr);
+    return ArrayJ{cle::tier1::gaussian_blur_func(dev->get(), src->get(), dst ? dst->get() : nullptr, sigmaX, sigmaY, sigmaZ)};
+}
+
+ArrayJ Tier1::addImageAndScalar(DeviceJ * dev, ArrayJ* src, ArrayJ* dst, float scalar)
+{
+    auto out = cle::tier1::add_image_and_scalar_func(dev->get(), src->get(), dst ? dst->get() : nullptr, scalar);
     return ArrayJ{out};
-}
-
-void Tier1::gaussianBlur(const DeviceJ &dev, const ArrayJ &src, const ArrayJ &dst, const float &sigmaX, const float &sigmaY, const float &sigmaZ)
-{
-    cle::tier1::gaussian_blur_func(dev.get(), src.get(), dst.get(), sigmaX, sigmaY, sigmaZ);
-}
-
-void Tier1::addImageAndScalar(const DeviceJ &dev, const ArrayJ &src, const ArrayJ &dst, const float &scalar)
-{
-    cle::tier1::add_image_and_scalar_func(dev.get(), src.get(), dst.get(), scalar);
 }

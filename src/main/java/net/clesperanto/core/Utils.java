bf4d54c558c2cb6dd0dd864d7556ccda095f7a28
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-08-01 11:14:43 +0200

    add import to utils

--- src/main/java/net/clesperanto/core/Utils.java
+++ src/main/java/net/clesperanto/core/Utils.java
@@ -3,29 +3,32 @@ package net.clesperanto.core;
 import java.util.ArrayList;
 
 import net.clesperanto._internals.jclic.StringVector;
+import net.clesperanto._internals.jclic.FloatVector;
+import net.clesperanto._internals.jclic.ArrayJVector;
+import net.clesperanto._internals.jclic.FloatVectorMap;
 
 public class Utils {
 
-	public static ArrayList<String> toArrayList(StringVector vector){
+	public static ArrayList<String> toArrayList(StringVector vector) {
 		ArrayList<String> arr = new ArrayList<String>();
-		for (int i = 0; i < vector.size(); i ++) {
+		for (int i = 0; i < vector.size(); i++) {
 			arr.add(vector.get(i));
 		}
 		return arr;
 	}
 
-	public static ArrayList<Float> toArrayList(FloatVector vector){
+	public static ArrayList<Float> toArrayList(FloatVector vector) {
 		ArrayList<Float> arr = new ArrayList<Float>();
-		for (int i = 0; i < vector.size(); i ++) {
+		for (int i = 0; i < vector.size(); i++) {
 			arr.add(vector.get(i));
 		}
 		return arr;
 	}
 
-	public static ArrayList<ArrayJ> toArrayList(ArrayJVector vector){
+	public static ArrayList<ArrayJ> toArrayList(ArrayJVector vector) {
 		ArrayList<ArrayJ> arr = new ArrayList<ArrayJ>();
-		for (int i = 0; i < vector.size(); i ++) {
-			DeviceJ device =  DeviceJ.getDeviceWithDefaultBackend(vector.get(i).getDevice(), "all");
+		for (int i = 0; i < vector.size(); i++) {
+			DeviceJ device = DeviceJ.getDeviceWithDefaultBackend(vector.get(i).getDevice(), "all");
 			arr.add(new ArrayJ(vector.get(i), device));
 		}
 		return arr;
@@ -33,8 +36,8 @@ public class Utils {
 
 	public static FloatVector toVector(ArrayList<Float> array) {
 		Float[] floatArray = new Float[array.size()];
-        floatArray = array.toArray(floatArray);
-        return new FloatVector(floatArray);
+		floatArray = array.toArray(floatArray);
+		return new FloatVector(floatArray);
 	}
 
 }

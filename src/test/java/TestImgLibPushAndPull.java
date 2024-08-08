ce9ff1d9c82d76d66976469c32e0a6375dbe7ace
carlosuc3m <100329787@alumnos.uc3m.es>
2024-08-08 09:38:14 +0200

    test imglib2 push and pull

---
+++ src/test/java/TestImgLibPushAndPull.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.integer.IntType;
+import net.clesperanto.core.ArrayJ;
+import net.clesperanto.core.DeviceJ;
+import net.clesperanto.imglib2.ImgLib2Converters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestImgLibPushAndPull {
+
+    @Test
+    public void testImgLibPushAndPull() {
+    	int[] flatVals = new int[18];
+    	for (int i = 0; i < flatVals.length; i ++)
+    		flatVals[i] = i;
+    	RandomAccessibleInterval<IntType> inputImg = ArrayImgs.ints(flatVals, new long[] {3, 3, 2});
+    	
+    	
+    	DeviceJ device = DeviceJ.getDefaultDevice();
+    	ArrayJ in = ImgLib2Converters.copyImgLib2ToArrayJ(inputImg, device, "buffer");
+    	RandomAccessibleInterval<IntType> outputImg = ImgLib2Converters.copyArrayJToImgLib2(in);
+    	
+    	Cursor<IntType> inCursor = inputImg.cursor();
+    	Cursor<IntType> outCursor = outputImg.cursor();
+    	
+    	while (inCursor.hasNext()) {
+    		assertEquals(inCursor.get().get(), outCursor.get().get());
+    		inCursor.next();
+    		outCursor.next();
+    	}
+    }
+}
